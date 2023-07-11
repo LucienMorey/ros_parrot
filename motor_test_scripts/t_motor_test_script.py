8fe8727839a9c1e7e53e9405326ceb14f2347d20
triped-pc <triped-pc>
2023-07-11 11:21:34 +1000

    update gitignore and commit test scripts

--- motor_test_scripts/t_motor_test_script.py
+++ motor_test_scripts/t_motor_test_script.py
@@ -11,7 +11,7 @@ import math
 
 if __name__ == "__main__":
 
-    motor_1_handle = TMotorManager(motor_type="AK80-9", motor_ID=7)
+    motor_1_handle = TMotorManager(motor_type="AK80-9", motor_ID=2)
     # motor_1_handle.invert_direction()
     # motor_1_handle.invert_encoder()
 
@@ -37,17 +37,20 @@ if __name__ == "__main__":
     else:
         print('failed to set control mode')
         exit(1)
-    motor_1_handle.set_output_torque_newton_meters(0)
+    # motor_1_handle.set_output_torque_newton_meters(0.1)
 
-    time.sleep(1)
-
+    # time.sleep(1)
     
     start_time = time.time()
-    while time.time() - start_time < 2:
-        print(motor_1_handle.get_output_angle_radians())
-        motor_1_handle.set_output_torque_newton_meters(-1)
+    while time.time() - start_time < 5:
+        print(motor_1_handle.get_output_angle_radians(), " ", motor_1_handle.get_output_velocity_radians_per_second(), " ", motor_1_handle.get_current_qaxis_amps())
+        # motor_1_handle.set_output_torque_newton_meters(0)
+        motor_1_handle.set_motor_torque_newton_meters(2)
         # motor_1_handle.set_output_torque_newton_meters(1.0 + math.sin(time.time()))
         time.sleep(0.01)
+    #print(motor_1_handle.device_info_string())
+
+    time.sleep(0.1)
     motor_1_handle.set_control_mode(TMotorControlState.IDLE)
 
     motor_1_handle.power_off()
--- motor_test_scripts/test_motor.py
+++ motor_test_scripts/test_motor.py
@@ -4,36 +4,41 @@ import time
 import math
 
 if __name__ == "__main__":
-    # motor = OdriveAxisHandle(63) # M0 left 
+    motor = OdriveAxisHandle(55) # M0 left
     # motor = OdriveAxisHandle(62) # M1 left
 
     # motor = OdriveAxisHandle(61) # right wrist 1
     # motor = OdriveAxisHandle(60) # right wrist 2
 
-    # motor = OdriveAxisHandle(58) # M0 neck
+    # motor = OdriveAxisHandle(56) # M0 neck
     # motor = OdriveAxisHandle(59) # M1 neck 
 
-    motor = OdriveAxisHandle(57) # tail motor
+    # motor = OdriveAxisHandle(57) # tail motor
 
     motor.clear_errors()
+    # motor.invert_motor()
 
     index_found = motor.find_index_pulse()
 
     if not motor.index_found:
-        print('Failed to find index pulse. Press ctrl+c to exit.')
-        exit(1)
+       print('Failed to find index pulse. Press ctrl+c to exit.')
+       exit(1)
 
     # motor.calibrate()
 
-    time.sleep(2)
+    time.sleep(5)
 
     motor.set_controller_mode(ControlMode.TORQUE_CONTROL, InputMode.PASSTHROUGH)
 
     motor.set_axis_state(AxisState.CLOSED_LOOP_CONTROL)
 
     last_time = time.time()
-    while time.time() - last_time < 2:
-        motor.set_torque(0.03)
+    set_torque = 0.0
+    torque_lim = 0.2
+    while time.time() - last_time < 5:
+        if set_torque <= torque_lim:
+            set_torque = set_torque + 0.001
+        motor.set_torque(set_torque)
         # motor.set_torque(0.1*math.sin(2*3.14*time.time()/2))
 
         print(
